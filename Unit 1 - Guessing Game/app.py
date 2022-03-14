be71cd90ea5c1ee4f67ea542be05dc14e589a02d
CA-Python <callumjanderson01@gmail.com>
2022-03-14 13:54:16 +0000

    Progress 2.0 - 14 March
    Main app done, statistics to be done.

--- Unit 1 - Guessing Game/app.py
+++ Unit 1 - Guessing Game/app.py
@@ -1,37 +1,60 @@
 import random
+import statistics
+
 
 def start_game():
+    ## Pre reqs ##
     answer = random.randint(1, 100)
-    print(answer)
-    guesses = 0
+    total = []
+    count = 0
+
+    ### Game Starts ###
     print(
         """
-            *****************************
+            ***********************************
 
-            Welcome to Guess That Number!
+               Welcome to Guess That Number!
 
-            *****************************
+            ***********************************
         """)
-    while True:
-        input('Pick a number from 1 to 100'
-              '\n'
-              '\n   ')
+    game = True
+    while game:
+        guess = input("Enter a number between 1 and 100"
+                      "\n")
+        ### Error try block ###
+        try:
+            guess = int(guess)
+            if guess not in range(1,100):
+                raise ValueError
+        except ValueError as err:
+            err = print("\nPlease enter a number that is between 1 and 100, try again")
+            start_game()
+
+        ### Guess conditions ###
+        if guess > answer:
+            print(f"{guess} is higher than the answer, try again...")
+            count += 1
+        elif guess < answer:
+            print(f"{guess} is lower than the answer, try again...")
+            count += 1
+        elif guess == answer:
+            count += 1
+            print(f"Well done you made {count} attempt(s) this time!\n")
+
+
+            confirm = input("Would you like to play again? Anything you say that is not 'Yes' will end the game.\n")
+            if confirm == "yes":
+                start_game()
+            else:
+                total.append(count)
+                print("Here is your overall stats after this game!"
+                      f"\nTotal Attempts: {total[0:]} \nAverage Score: \n"
+                      "\nThank you for playing! Goodbye!")
+
+            break
+
+
+
+
 
 start_game()
-
-"""Psuedo-code Hints
-
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-
-    ( You can add more features/enhancements if you'd like to. )
-    """
